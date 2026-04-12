80105055149c822e538b8d09d40afd3bc5db7e52
vnxkaito <81317627+vnxkaito@users.noreply.github.com>
2026-04-12 18:50:11 +0300

    created role repository

---
+++ tms/tms/src/main/java/com/ga/tms/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.ga.tms.repository;
+
+import com.ga.tms.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
