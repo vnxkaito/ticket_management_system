dc8bc88d6a8adf751039773e5472b5319abbf289
vnxkaito <81317627+vnxkaito@users.noreply.github.com>
2026-04-12 19:01:34 +0300

    added notification job repository

---
+++ tms/tms/src/main/java/com/ga/tms/repository/NotificationJobRepository.java
@@ -0,0 +1,7 @@
+package com.ga.tms.repository;
+
+import com.ga.tms.model.NotificationJob;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationJobRepository extends JpaRepository<NotificationJob, Long> {
+}
