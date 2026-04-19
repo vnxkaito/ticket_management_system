f664bea74e2cf165dbba8cf3f844fb4177066e2d
vnxkaito <81317627+vnxkaito@users.noreply.github.com>
2026-04-19 17:19:39 +0300

    updateTeam

--- tms/tms/src/main/java/com/ga/tms/controller/TeamController.java
+++ tms/tms/src/main/java/com/ga/tms/controller/TeamController.java
@@ -4,10 +4,7 @@ import com.ga.tms.model.Team;
 import com.ga.tms.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,6 +29,11 @@ public class TeamController {
     @GetMapping("/{id}")
     public ResponseEntity<Team> getTeamById(@PathVariable Long id) {
         return ResponseEntity.ok(teamService.getTeamById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Team> updateTeam(@PathVariable Long id, @RequestBody Team team) {
+        return ResponseEntity.ok(teamService.updateTeam(id, team));
     }
 
 
