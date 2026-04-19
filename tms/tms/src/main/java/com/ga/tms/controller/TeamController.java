23454c6cd20b0084f0c4361973cae7ac77bfb57d
vnxkaito <81317627+vnxkaito@users.noreply.github.com>
2026-04-19 17:15:12 +0300

    getAllTeams

--- tms/tms/src/main/java/com/ga/tms/controller/TeamController.java
+++ tms/tms/src/main/java/com/ga/tms/controller/TeamController.java
@@ -4,8 +4,11 @@ import com.ga.tms.model.Team;
 import com.ga.tms.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 public class TeamController {
     private final TeamService teamService;
@@ -20,5 +23,10 @@ public class TeamController {
         return ResponseEntity.ok(teamService.createTeam(team));
     }
 
-    
+    @GetMapping
+    public ResponseEntity<List<Team>> getAllTeams() {
+        return ResponseEntity.ok(teamService.getAllTeams());
+    }
+
+
 }
