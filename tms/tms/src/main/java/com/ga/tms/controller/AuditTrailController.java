61424c05c70e4e16d5d49d2030a286d5c98c17ef
vnxkaito <81317627+vnxkaito@users.noreply.github.com>
2026-04-19 16:37:44 +0300

    created getAuditTrailByTicket

--- tms/tms/src/main/java/com/ga/tms/controller/AuditTrailController.java
+++ tms/tms/src/main/java/com/ga/tms/controller/AuditTrailController.java
@@ -1,9 +1,15 @@
 package com.ga.tms.controller;
 
+import com.ga.tms.model.TicketAuditEvent;
 import com.ga.tms.service.TicketAuditEventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/audit")
@@ -14,4 +20,11 @@ public class AuditTrailController {
     public AuditTrailController(TicketAuditEventService ticketAuditEventService) {
         this.ticketAuditEventService = ticketAuditEventService;
     }
+
+    @GetMapping("/tickets/{ticketId}")
+    public ResponseEntity<List<TicketAuditEvent>> getAuditTrailByTicket(@PathVariable Long ticketId) {
+        return ResponseEntity.ok(ticketAuditEventService.getAuditTrailByTicket(ticketId));
+    }
+
+
 }
